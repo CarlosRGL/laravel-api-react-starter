18cf7e1c05ac31b62a09a15587db82a10cc53161
CarlosRGL <carlosrgl@quai13.com>
2023-02-17 10:41:49 +0100

    👌 IMPROVE: added react route and define routes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <div className="App">App</div>;
+  return <div className="App">App</div>;
 }
 
 export default App;
---
+++ react/src/views/Notfound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Notfound() {
+  return <div>404 not found</div>;
+}
+
+export default Notfound;
