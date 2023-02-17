ff6fa4ca6875646dd0c637fb3793ac77d84c8adb
CarlosRGL <carlosrgl@quai13.com>
2023-02-17 16:54:36 +0100

    👌 IMPROVE: added axios config

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -71,7 +71,7 @@ function Signup() {
             <input
               id="confirm-password"
               name="confirm-password"
-              type="confirm-password"
+              type="password"
               autoComplete="current-password"
               required
               className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
