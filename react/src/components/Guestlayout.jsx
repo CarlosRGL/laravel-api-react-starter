d057df9b67f9c35be6dcdf9fc7c375c32e10ac99
CarlosRGL <carlosrgl@quai13.com>
2023-02-17 14:47:00 +0100

    👌 IMPROVE: added defaut style

--- react/src/components/Guestlayout.jsx
+++ react/src/components/Guestlayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStateContext } from "../context/ContextProvider";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function Guestlayout() {
   const { token } = useStateContext();
