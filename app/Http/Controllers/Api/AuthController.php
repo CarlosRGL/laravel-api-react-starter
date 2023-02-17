6af300599800732d89820fd66e1a9b91ba1ee53b
CarlosRGL <carlosrgl@quai13.com>
2023-02-17 17:22:15 +0100

    👌 IMPROVE: added controllers

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller {
+
+	public function signup( SignupRequest $request ) {
+		$data = $request->validated();
+		$user = User::create(
+			array(
+				'name'     => $data['name'],
+				'email'    => $data['email'],
+				'password' => bcrypt( $data['password'] ),
+			)
+		);
+
+		$token = $user->createToken( 'main' )->plainTextToken;
+
+		return response( compact( 'user', 'token' ), 201 );
+	}
+
+	public function login( LoginRequest $request ) {
+		$credentials = $request->validated();
+
+		if ( ! Auth::attempt( $credentials ) ) {
+			return response( array( 'message' => 'Invalid login details' ), 401 );
+		}
+		/** @var User $user */
+		$user  = Auth::user();
+		$token = $user->createToken( 'main' )->plainTextToken;
+		return response( compact( 'user', 'token' ), 201 );
+	}
+
+	public function logout( Request $request ) {
+		/** @var User $user */
+		$user = $request->user();
+		$user->currentAccessToken()->delete();
+		return response( array( 'message' => 'Logged out' ), 200 );
+	}
+}
---
+++ app/Http/Requests/SignupRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignupRequest extends FormRequest {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool {
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+	 */
+	public function rules(): array {
+		return array(
+			'name'     => 'required|string|max:55',
+			'email'    => 'required|email|unique:users,email',
+			'password' => array(
+				'required',
+				'string',
+				'min:8',
+				Password::min( 8 )->letters()->numbers(),
+			),
+		);
+	}
+}
