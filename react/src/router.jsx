ad3a0eb70680ae5263075baded8c37e53fa619dd
CarlosRGL <carlosrgl@quai13.com>
2023-02-17 10:51:29 +0100

    👌 IMPROVE: added routes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,22 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Users from "./views/Users";
 import Notfound from "./views/Notfound";
+import Defaultlayout from "./components/Defaultlayout";
+import Guestlayout from "./components/Guestlayout";
+import Dashboard from "./views/Dashboard";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />,
+    path: "/",
+    element: <Defaultlayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
   {
-    path: "/signup",
-    element: <Signup />,
+    path: "/",
+    element: <Guestlayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
   },
-  {
-    path: "/users",
-    element: <Users />,
-  },
+
   {
     path: "*",
     element: <Notfound />,
