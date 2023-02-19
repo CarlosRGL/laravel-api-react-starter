307abcac42a807f127fa9dbb069888239f88c762
CarlosRGL <carlosrgl@quai13.com>
2023-02-19 19:12:53 +0100

    👌 IMPROVE: edit user form

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axiosClient from "../api/axios-client";
 
 function UserForm({ key }) {
+  const id = useParams().id;
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({
+    id: null,
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  if (id) {
+    // fetch user
+    useEffect(() => {
+      setLoading(true);
+      axiosClient
+        .get(`/users/${id}`)
+        .then(({ data }) => {
+          setUser(data.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }, []);
+  }
+
   return (
     <form action="#" method="POST">
       <div className="shadow sm:overflow-hidden sm:rounded-md">
         <div className="space-y-6 bg-white py-6 px-4 sm:p-6">
           <div>
             <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Personal Information
+              {user.id && <span>Modifier l'utilisateur {user.name}</span>}
+              {!user.id && <span>Nouveau utilisateur</span>}
             </h3>
-            <p className="mt-1 text-sm text-gray-500">
-              Use a permanent address where you can recieve mail.
-            </p>
           </div>
 
           <div className="grid grid-cols-6 gap-6">
@@ -28,6 +54,7 @@ function UserForm({ key }) {
                 id="first-name"
                 autoComplete="given-name"
                 className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                value={user.name}
               />
             </div>
             <div className="col-span-6 sm:col-span-3">
@@ -43,6 +70,7 @@ function UserForm({ key }) {
                 id="email"
                 autoComplete="email"
                 className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                value={user.email}
               />
             </div>
             <div className="col-span-6 sm:col-span-3">
@@ -57,6 +85,7 @@ function UserForm({ key }) {
                 name="password"
                 id="password"
                 className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                value={user.password}
               />
             </div>
             <div className="col-span-6 sm:col-span-3">
@@ -71,6 +100,7 @@ function UserForm({ key }) {
                 name="confirm-password"
                 id="confirm-password"
                 className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                value={user.password_confirmation}
               />
             </div>
           </div>
